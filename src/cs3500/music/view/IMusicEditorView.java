8c3d580ce4811e3dc0bc7ddf31ce12824e8dec4d
Justin Knichel <jtk21351@gmail.com>
2016-11-02 21:12:07 -0400

    Add initial views
    Added initial versions of the IMusicEditorView interface, and its first
    implementation: the console view.

---
+++ src/cs3500/music/view/IMusicEditorView.java
@@ -0,0 +1,29 @@
+package cs3500.music.view;
+
+import java.util.List;
+import java.util.Map;
+
+import cs3500.music.model.IMusicEditorModel;
+import cs3500.music.model.MusicNote;
+
+/**
+ * The view interface for the music editor. Should implement all methods the controller needs to
+ * invoke on the view.
+ */
+public interface IMusicEditorView {
+  /**
+   * Make the view visible. Should be called in a controller's initialization method after the view
+   * has already been constructed.
+   */
+  void makeVisible();
+
+  /**
+   * Command for the view to redraw itself. Takes in a bunch of info about the state of the model
+   * in order to perform its task.
+   * @param noteRange a list of notes from lowest to highest note.
+   * @param noteStartingBeats a map mapping each beat to a note that starts on it.
+   * @param noteContinuationBeats a map mapping each beat to a note that continues through it.
+   */
+  void refresh(List<MusicNote> noteRange, Map<Integer, List<MusicNote>> noteStartingBeats,
+               Map<Integer, List<MusicNote>> noteContinuationBeats, int songLength);
+}
---
+++ src/cs3500/music/view/MusicEditorConsoleView.java
@@ -0,0 +1,91 @@
+package cs3500.music.view;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import cs3500.music.model.IMusicEditorModel;
+import cs3500.music.model.MusicNote;
+
+/**
+ * A console view for a IMusicEditorModel.
+ */
+public class MusicEditorConsoleView implements IMusicEditorView {
+  IMusicEditorModel model;
+  final Appendable out = System.out;
+
+  @Override
+  public void makeVisible() {
+    return;
+  }
+
+  @Override
+  public void refresh(List<MusicNote> noteRange, Map<Integer, List<MusicNote>> noteStartingBeats,
+                      Map<Integer, List<MusicNote>> noteContinuationBeats, int songLength) {
+    // figure out how many spaces we need to pad to, based on the total number of beats in song
+    int lengthToPadNotes = String.valueOf(songLength).length();
+    try {
+      out.append(noteHeaderRow(noteRange, lengthToPadNotes) + "\n");
+      for (int i = 0; i <= songLength; i++) {
+        out.append(String.format("%1$" + String.valueOf(lengthToPadNotes) + "d", i));
+        out.append("  " + beatRowToString(noteRange, noteStartingBeats.get(i),
+                noteContinuationBeats.get(i)) + "\n");
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private String noteHeaderRow(List<MusicNote> noteRange, int lengthToPadNotes) {
+    // Create the string of whitespace to block off column for beats before starting note headerRowString
+    String formatString = "%-" + String.valueOf(lengthToPadNotes) + "s";
+    String headerRowString = String.format(formatString, " ");
+
+    for (MusicNote note : noteRange) {
+      headerRowString += centerNoteText(note.toString());
+    }
+    return headerRowString;
+  }
+
+  private String beatRowToString(List<MusicNote> noteRange, List<MusicNote> noteStarts,
+                                 List<MusicNote> noteContinues) {
+    String beatRow = "";
+
+    for (MusicNote note : noteRange) {
+      for (MusicNote start : noteStarts) {
+        if (note.getPitch() == start.getPitch() && note.getOctave() == start.getOctave()) {
+          beatRow += "X";
+          break;
+        }
+      }
+      if (beatRow.length() > 0 && beatRow.charAt(beatRow.length() - 1) == 'X') {
+        beatRow += "    ";
+        continue;
+      }
+      for (MusicNote continues : noteContinues) {
+        if (note.getPitch() == continues.getPitch() && note.getOctave() == continues.getOctave()) {
+          beatRow += "|";
+          break;
+        }
+      }
+      if (beatRow.length() > 0 && beatRow.charAt(beatRow.length() - 1) == '|') {
+        beatRow += "    ";
+      } else {
+        beatRow += "     ";
+      }
+    }
+    return beatRow;
+  }
+
+  private String centerNoteText(String noteString) {
+    if (noteString.length() == 2) {
+      return "  " + noteString + " ";
+    } else if (noteString.length() == 3) {
+      return " " + noteString + " ";
+    } else if (noteString.length() == 4) {
+      return " " + noteString;
+    } else {
+      return noteString;
+    }
+  }
+}
