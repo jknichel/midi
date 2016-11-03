08203b39fe08ba842abbc6c3395951acbe42cd28
Justin Knichel <jtk21351@gmail.com>
2016-11-02 21:10:36 -0400

    Add cs3500.music.controller
    Add the controller directory, and initial implementations of its component
    files. Currently just holds the Controller interface and its single
    implementation.

---
+++ src/cs3500/music/controller/IMusicEditorController.java
@@ -0,0 +1,11 @@
+package cs3500.music.controller;
+
+/**
+ * Interface for the MusicEditor controller.
+ */
+public interface IMusicEditorController {
+  /**
+   * Start the program, basically. Give control to the controller, start up view based on model.
+   */
+  void go();
+}
---
+++ src/cs3500/music/controller/MusicEditorController.java
@@ -0,0 +1,23 @@
+package cs3500.music.controller;
+
+import cs3500.music.model.IMusicEditorModel;
+import cs3500.music.view.IMusicEditorView;
+
+/**
+ * Implementation of a MusicEditor controller.
+ */
+public class MusicEditorController implements IMusicEditorController {
+  private IMusicEditorModel model;
+  private IMusicEditorView view;
+
+  public MusicEditorController(IMusicEditorModel model, IMusicEditorView view) {
+    this.model = model;
+    this.view = view;
+  }
+
+  @Override
+  public void go() {
+    this.view.refresh(model.noteRange(), model.noteStartingBeats(), model.noteContinuationBeats(),
+            model.getLength());
+  }
+}
