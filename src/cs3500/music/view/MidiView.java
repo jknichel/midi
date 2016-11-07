db04f3ba9854f4e003ff02adc1b1c6406695cd68
Justin Knichel <jtk21351@gmail.com>
2016-11-07 00:05:27 -0500

    Refactor MidiView to use Receiver.send()

--- src/cs3500/music/view/MidiView.java
+++ src/cs3500/music/view/MidiView.java
@@ -7,10 +7,13 @@ import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiEvent;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Receiver;
 import javax.sound.midi.Sequence;
 import javax.sound.midi.Sequencer;
 import javax.sound.midi.ShortMessage;
+import javax.sound.midi.Synthesizer;
 import javax.sound.midi.Track;
+import javax.sound.midi.Transmitter;
 
 import cs3500.music.model.MusicNote;
 
@@ -31,7 +34,7 @@ public class MidiView implements IMusicEditorView {
   @Override
   public void refresh(List<MusicNote> noteRange, Map<Integer, List<MusicNote>> noteStartingBeats,
                       Map<Integer, List<MusicNote>> noteContinuationBeats, int songLength) {
-    try {
+    /*try {
       Sequencer sequencer = MidiSystem.getSequencer();
       sequencer.open();
 
@@ -64,6 +67,33 @@ public class MidiView implements IMusicEditorView {
       return;
     } catch (InvalidMidiDataException e) {
       return;
+    }*/
+
+    Sequencer seq;
+    Transmitter seqTrans;
+    Synthesizer synth;
+    Receiver synthRcvr;
+    try {
+      seq = MidiSystem.getSequencer();
+      seqTrans = seq.getTransmitter();
+      synth = MidiSystem.getSynthesizer();
+      synthRcvr = synth.getReceiver();
+      seqTrans.setReceiver(synthRcvr);
+      synth.open();
+
+      for (int i = 0; i <= songLength; i++) {
+        if (noteStartingBeats.containsKey(i)) {
+          for (MusicNote note : noteStartingBeats.get(i)) {
+            playNote(i, note, synthRcvr);
+          }
+        }
+      }
+
+      Thread.sleep(100000);
+    } catch (MidiUnavailableException e) {
+      // handle or throw exception
+    } catch (InterruptedException e) {
+      e.printStackTrace();
     }
   }
 
@@ -79,6 +109,21 @@ public class MidiView implements IMusicEditorView {
 
       track.add(new MidiEvent(onMsg, beat));
       track.add(new MidiEvent(offMsg, beat + note.getTotalDuration()));
+    } catch (InvalidMidiDataException e) {}
+  }
+
+  private void playNote(int beat, MusicNote note, Receiver receiver) {
+    ShortMessage onMsg = new ShortMessage();
+    ShortMessage offMsg = new ShortMessage();
+
+    int midiCompatiblePitch = (note.getPitch().getScaleIndex() - 1) + (note.getOctave() * 12) + 12;
+
+    try {
+      onMsg.setMessage(ShortMessage.NOTE_ON, 10, midiCompatiblePitch, note.getVolume());
+      offMsg.setMessage(ShortMessage.NOTE_OFF, 10, midiCompatiblePitch, note.getVolume());
+
+      receiver.send(onMsg, beat * tempo);
+      receiver.send(offMsg, (beat + note.getTotalDuration()) * tempo);
     } catch (InvalidMidiDataException e) {}
   }
 }
